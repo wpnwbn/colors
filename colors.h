724816df7d23bb21a141ab4887a908484d6b241c
rukos <58227860+wpnwbn@users.noreply.github.com>
2022-02-20 12:09:13 +0300

    ADD: colors.h
    Header file containing macro definitions with colors' names to easily apply them to your text.

---
+++ colors.h
@@ -0,0 +1,43 @@
+#ifndef __COLORS__H__
+#define __COLORS__H__
+
+/* Colors:              Styles:
+   N - black (night)    N - normal
+   R - red              B - bold
+   G - green            T - change tone
+   Y - yellow           I - italic
+   B - blue             U - underline
+   M - magenta
+   C - cyan
+   W - white
+*/
+
+// Text colors (TC)
+#define TCN "\033[30m"
+#define TCR "\033[31m"
+#define TCG "\033[32m"
+#define TCY "\033[33m"
+#define TCB "\033[34m"
+#define TCM "\033[35m"
+#define TCC "\033[36m"
+#define TCW "\033[37m"
+// Background colors (BC)
+#define BCN "\033[40m"
+#define BCR "\033[41m"
+#define BCG "\033[42m"
+#define BCY "\033[43m"
+#define BCB "\033[44m"
+#define BCM "\033[45m"
+#define BCC "\033[46m"
+#define BCW "\033[47m"
+// Text styles (TS)
+#define TSN "\033[0m"
+#define TSB "\033[1m"
+#define TST "\033[2m"
+#define TSI "\033[3m"
+#define TSU "\033[4m"
+// Reset color: normal style, white text color, black background
+#define RC "\033[0;37;0m"
+
+#endif
+
